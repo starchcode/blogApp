7e54364bdef67f9a7a000ca111753d3ad72a7287
Dave <64050401+starchcode@users.noreply.github.com>
2022-06-29 23:45:53 +0100

    posts CRUD basics

--- server/api/posts.js
+++ server/api/posts.js
@@ -1,17 +1,40 @@
 const posts = require("express").Router();
+const Post = require("../model/Post");
 const User = require("../model/User");
 
 posts.get("/", async (req, res) => {
-    res.send("Here are all your posts!")
-
+    const posts = await Post.findAll();
+    res.send(posts);
 })
 
-// some practice
-// posts.get("/:id", async (req, res) => {
-//     const requestedId = req.params.id;
-//     const user = await User.findOne({ where: { id: requestedId }});
-//     res.send(user);
-// });
+posts.post("/", async (req, res) => {
+    // body must contain: { title, body, user_id } 
+     const post = await Post.create(req.body)
+     res.send(post);
+})
 
+posts.put("/:postId", async (req, res) => {
+    // body must contain: { title, body, user_id } 
+    const postId = req.params.postId;
+    const post = await Post.findOne({ where : { id: postId }});
+    
+    Object.keys(req.body).forEach(key => {
+        post[key] = req.body[key];
+      });
 
+    await post.save();
+    res.send(post);
+      
+});
+
+posts.delete("/:postId", async (req, res) => {
+    const postId = req.params.postId;
+    await Post.destroy({ where: { id: postId }});
+    res.send('removed');
+})
 module.exports = posts;
+
+
+//TODO: 
+    // refactor using DRY
+    // start testing and continue with TDD
